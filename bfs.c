1764a214badebf6758e641511b2fb691c186ad89
Gourishankari <104970927+Gourishankari@users.noreply.github.com>
2023-07-22 08:24:50 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,137 +1,103 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
-// Structure for a binary tree node
+#define MAX_VERTICES 100
+
+// Structure to represent a node in the graph
 struct Node {
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node* next;
+};
+
+// Structure to represent the graph
+struct Graph {
+    int numVertices;
+    struct Node* adjLists[MAX_VERTICES];
 };
 
 // Function to create a new node
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
+    newNode->next = NULL;
     return newNode;
 }
 
-// Function to insert a node in BST
-struct Node* insert(struct Node* root, int data) {
-    if (root == NULL) {
-        return createNode(data);
-    } else {
-        if (data <= root->data) {
-            root->left = insert(root->left, data);
-        } else {
-            root->right = insert(root->right, data);
+// Function to create a graph with n vertices
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    for (int i = 0; i < vertices; ++i) {
+        graph->adjLists[i] = NULL;
+    }
+
+    return graph;
+}
+
+// Function to add an edge to the graph
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    // For undirected graph, add an edge from dest to src as well
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+// Function to perform Breadth-First Search traversal
+void BFS(struct Graph* graph, int startVertex) {
+    bool visited[MAX_VERTICES] = {false};
+    int queue[MAX_VERTICES];
+    int front = -1, rear = -1;
+
+    // Mark the startVertex as visited and enqueue it
+    visited[startVertex] = true;
+    queue[++rear] = startVertex;
+
+    while (front < rear) {
+        // Dequeue a vertex from the queue and process it
+        int currentVertex = queue[++front];
+        printf("%d ", currentVertex);
+
+        // Traverse all adjacent vertices of the currentVertex
+        struct Node* temp = graph->adjLists[currentVertex];
+        while (temp) {
+            int adjVertex = temp->data;
+            if (!visited[adjVertex]) {
+                visited[adjVertex] = true;
+                queue[++rear] = adjVertex;
+            }
+            temp = temp->next;
         }
-        return root;
     }
-}
-
-// Function to delete a node from BST
-struct Node* delete(struct Node* root, int data) {
-    if (root == NULL) {
-        return root;
-    } else if (data < root->data) {
-        root->left = delete(root->left, data);
-    } else if (data > root->data) {
-        root->right = delete(root->right, data);
-    } else {
-        // Case 1: No child or only one child
-        if (root->left == NULL) {
-            struct Node* temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            struct Node* temp = root->left;
-            free(root);
-            return temp;
-        }
-
-        // Case 2: Two children
-        struct Node* temp = root->right;
-        while (temp->left != NULL) {
-            temp = temp->left;
-        }
-        root->data = temp->data;
-        root->right = delete(root->right, temp->data);
-    }
-    return root;
-}
-
-// Function for in-order traversal (left-root-right)
-void inOrder(struct Node* root, FILE* fp) {
-    if (root == NULL) {
-        return;
-    }
-    inOrder(root->left, fp);
-    fprintf(fp, "%d ", root->data);
-    inOrder(root->right, fp);
-}
-
-// Function for pre-order traversal (root-left-right)
-void preOrder(struct Node* root, FILE* fp) {
-    if (root == NULL) {
-        return;
-    }
-    fprintf(fp, "%d ", root->data);
-    preOrder(root->left, fp);
-    preOrder(root->right, fp);
-}
-
-// Function for post-order traversal (left-right-root)
-void postOrder(struct Node* root, FILE* fp) {
-    if (root == NULL) {
-        return;
-    }
-    postOrder(root->left, fp);
-    postOrder(root->right, fp);
-    fprintf(fp, "%d ", root->data);
-}
-
-// Function to create a log file and call traversal functions
-void logTraversal(struct Node* root) {
-    FILE* fp = fopen("bst_traversal.log", "w");
-    if (fp == NULL) {
-        printf("Error opening the log file.");
-        return;
-    }
-
-    fprintf(fp, "In-order traversal: ");
-    inOrder(root, fp);
-    fprintf(fp, "\n");
-
-    fprintf(fp, "Pre-order traversal: ");
-    preOrder(root, fp);
-    fprintf(fp, "\n");
-
-    fprintf(fp, "Post-order traversal: ");
-    postOrder(root, fp);
-    fprintf(fp, "\n");
-
-    fclose(fp);
 }
 
 int main() {
-    struct Node* root = NULL;
+    int vertices, edges;
+    printf("Enter the number of vertices in the graph: ");
+    scanf("%d", &vertices);
 
-    // Insert nodes in BST
-    root = insert(root, 50);
-    root = insert(root, 30);
-    root = insert(root, 20);
-    root = insert(root, 40);
-    root = insert(root, 70);
-    root = insert(root, 60);
-    root = insert(root, 80);
+    struct Graph* graph = createGraph(vertices);
 
-    // Delete node from BST
-    root = delete(root, 20);
+    printf("Enter the number of edges in the graph: ");
+    scanf("%d", &edges);
 
-    // Log traversal in a file
-    logTraversal(root);
+    int src, dest;
+    printf("Enter the edges (src, dest):\n");
+    for (int i = 0; i < edges; ++i) {
+        scanf("%d %d", &src, &dest);
+        addEdge(graph, src, dest);
+    }
+
+    int startVertex;
+    printf("Enter the starting vertex for BFS traversal: ");
+    scanf("%d", &startVertex);
+
+    printf("BFS Traversal starting from vertex %d: ", startVertex);
+    BFS(graph, startVertex);
 
     return 0;
 }
